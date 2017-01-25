a2beed38d1b8890f292c109ded6ec9f49e2214ab
Matthew Spada <kepoly@Matthews-MacBook-Pro-2.local>
2017-01-24 21:53:59 -0500

    Routes cleanup

--- src/app.js
+++ src/app.js
@@ -36,70 +36,10 @@ app.config(['$routeProvider', '$locationProvider',
                 controller: 'dashboardCtrl',
                 templateUrl: '../templates/dashboard/dashboard.php'
             })
-            .when('/inventory', {
-                title: 'Home',
-                controller: 'inventoryCtrl',
-                templateUrl: '../templates/inventory/inventory.php'
-            })
-            .when('/customers', {
-                title: 'Home',
-                controller: 'customersCtrl',
-                templateUrl: '../templates/customers/customers.php'
-            })
-            .when('/projects', {
-                title: 'Home',
-                controller: 'projectsCtrl',
-                templateUrl: '../templates/projects/projects.php'
-            })
-            .when('/suppliers', {
-                title: 'Home',
-                controller: 'suppliersCtrl',
-                templateUrl: '../templates/suppliers/suppliers.php'
-            })
-            .when('/finances', {
-                title: 'Home',
-                controller: 'financesCtrl',
-                templateUrl: '../templates/finances/finances.php'
-            })
-            .when('/showcase', {
-                title: 'Home',
-                controller: 'showcaseCtrl',
-                templateUrl: '../templates/showcase/showcase.php'
-            })
             .when('/users', {
                 title: 'Home',
                 controller: 'usersCtrl',
                 templateUrl: '../templates/users/users.php'
-            })
-            .when('/settings', {
-                title: 'Home',
-                controller: 'settingsCtrl',
-                templateUrl: '../templates/settings/settings.php'
-            })
-            .when('/permissions', {
-                title: 'Home',
-                controller: 'permissionsCtrl',
-                templateUrl: '../templates/permissions/permissions.php'
-            })
-            .when('/logs', {
-                title: 'Home',
-                controller: 'logsCtrl',
-                templateUrl: '../templates/logs/logs.php'
-            })
-            .when('/databases', {
-                title: 'Home',
-                controller: 'databasesCtrl',
-                templateUrl: '../templates/databases/databases.php'
-            })
-            .when('/user-preferences', {
-                title: 'Home',
-                controller: 'userPreferencesCtrl',
-                templateUrl: '../templates/user-preferences/user-preferences.php'
-            })
-            .when('/administration-profile', {
-                title: 'Home',
-                controller: 'administrationProfileCtrl',
-                templateUrl: '../templates/administration-profile/administration-profile.php'
             })
             .when('/profile/:userName', {
                 title: 'Profile',
